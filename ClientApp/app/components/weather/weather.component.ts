903979aac8201001f6261b90559a2694d55fa114
Ranu <O@O.net>
2017-02-08 16:44:23 +0000

    Add Weather Component

---
+++ ClientApp/app/components/weather/weather.component.ts
@@ -0,0 +1,19 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'weather',
+    template: require('./weather.component.html')
+})
+export class WeatherComponent {
+    public weather: Weather;
+
+    constructor() {
+        this.weather = { temp: "12", summary: "Barmy", city: "London" };
+    }
+}
+
+interface Weather {
+    temp: string;
+    summary: string;
+    city: string;
+}
